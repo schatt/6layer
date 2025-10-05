9354bf0dfe73febbfe2207b952769324cfa50edf
Drew Schatt <schatt@schatt.com>
2025-10-05 08:39:53 -0700

    Bump Package.swift header to v3.4.4 (DynamicFormView label duplication fix)

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// SixLayerFramework v3.4.1 - Cross-Platform Text Content Type Improvements (Fixed)
+// SixLayerFramework v3.4.4 - DynamicFormView Label Duplication Fix
 
 import PackageDescription
 
